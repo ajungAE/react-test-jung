f3b6ae7d3536fa28429ab9b758f68c03af6bd7aa
Alexander Jung <AJung@o365.comhard.de>
2025-04-29 21:55:02 +0000

    Add TodoList component for managing to-do items

---
+++ src/TodoList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+ 
+function TodoList() {
+  const [todos, setTodos] = React.useState([]);
+  const [newTodo, setNewTodo] = React.useState("");
+ 
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo(""); // Eingabefeld zurücksetzen
+    }
+  };
+ 
+  return (
+    <div>
+      <h3>To-Do Liste</h3>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Neue Aufgabe hinzufügen"
+      />
+      <button onClick={addTodo}>Hinzufügen</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
